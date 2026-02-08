27122d90d500f64eae9836cf941922b3796a629b
Lea Horn <git@autoit4you.de>
2026-02-09 00:21:10 +0100

    Forgot tan format

---
+++ AV.FinTS.Raw/Codes/TanFormat.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AV.FinTS.Raw.Codes
+{
+    public enum TanFormat
+    {
+        NUMERIC = 1,
+        ALPHA_NUMERIC = 2
+    }
+}
